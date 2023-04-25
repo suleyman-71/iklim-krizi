c972176b896eb07a0c1254600daf4392f8f46ce2
Suleyman Yollyyev <suleymanyollyyew@gmail.com>
2023-04-25 16:42:06 +0300

    snapshot

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public SnapshotCamera snapCam;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            snapCam.CallTakeSnapshot();
+        }
+    }
+}
